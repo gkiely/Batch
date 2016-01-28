e9fd2ad7f3a25c59a7817f6f7113589ef807c21b
Grant <grant.kiely@gmail.com>
2016-01-29 08:50:01 +1100

    switch back to webpack-stream, waiting on fixt at https://github.com/shama/webpack-stream/issues/67#issuecomment-175688245

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,8 @@ var fileInclude   = require('gulp-file-include'),
     // sourcemaps    = require('gulp-sourcemaps'),
     gutil         = require('gulp-util');
     webserver     = require('gulp-webserver'),
-    webpack       = require('webpack'),
-    // webpack       = require('webpack-stream'),
+    // webpack       = require('webpack'),
+    webpack       = require('webpack-stream'),
     //Build only
     extend        = require('extend'),
     gulpif        = require('gulp-if'),
@@ -54,28 +54,28 @@ gulp.task('server', function(){
 ==================================*/
 gulp.task('webpack:dev', function(cb){
   var wpConfig = prod ? require('./webpack.prod.js') : require('./webpack.config.js');  
+  // wpConfig.entry = './src/js/app/App';
   // wpConfig.output = {
-  //   filename: 'bundle.js'
+  //   filename: 'bundle.js',
+  //   path: config.js.dist
   // };
-  wpConfig.entry = './src/js/app/App';
+  // webpack(wpConfig, function(err, stats){
+  //   if(err) throw new gutil.PluginError("webpack", err);
+  //   else{
+  //     console.log('wepback updated successfully');
+  //   }
+  //   // gutil.log("[webpack]", stats.toString({
+  //   //     // output options
+  //   // }));
+  // });
+  
   wpConfig.output = {
-    filename: 'bundle.js',
-    path: config.js.dist
-  };
-  webpack(wpConfig, function(err, stats){
-    if(err) throw new gutil.PluginError("webpack", err);
-    else{
-      console.log('wepback updated successfully');
-    }
-    // gutil.log("[webpack]", stats.toString({
-    //     // output options
-    // }));
-  });
-   
-  // return gulp.src(config.js.input)
-  // .pipe(webpack(wpConfig))
-  // .on('error', handleError)
-  // .pipe(gulp.dest(config.js.dist))
+    filename: 'bundle.js'
+  };   
+  return gulp.src(config.js.input)
+  .pipe(webpack(wpConfig))
+  .on('error', handleError)
+  .pipe(gulp.dest(config.js.dist))
 });
 
 
